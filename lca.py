117c63512dfd70d3b97bfa8787719a2552bf289c
Sean Candon <candons@tcd.ie>
2018-09-22 18:50:47 +0100

    added findPath method

--- lca.py
+++ lca.py
@@ -4,3 +4,18 @@ class Node:
         self.key = key
         self.left = None
         self.right = None
+
+
+def findPath(root, path, k):
+
+    path.append(root.key)
+
+    if root.key == k:
+        return True
+
+    if((root.left!=None and findPath(root.left, path, k)) or
+       (root.right!=None and findPath(root.right, path, k))):
+        return True
+
+    path.pop()
+    return False
