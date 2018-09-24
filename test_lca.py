2f694c48e70d144400ff316c02e9db20f9b0fa8e
Sean Candon <candons@tcd.ie>
2018-09-24 23:33:44 +0100

    added test_PrintBreadthFirst method

--- test_lca.py
+++ test_lca.py
@@ -35,3 +35,15 @@ class TestLCA(unittest.TestCase):
         ans = []
         lca.printDepthFirst(root, ans)
         self.assertEqual(ans, [1, 2, 4, 5, 3, 6, 7])
+
+    def test_PrintBreadthFirst(self):
+        root = lca.Node(1)
+        root.left = lca.Node(2)
+        root.right = lca.Node(3)
+        root.left.left = lca.Node(4)
+        root.left.right = lca.Node(5)
+        root.right.left = lca.Node(6)
+        root.right.right = lca.Node(7)
+        ans = []
+        lca.printDepthFirst(root, ans)
+        self.assertEqual(ans, [1, 2, 3, 4, 5, 6, 7])
