0e172585821e4b60bafd824197ec832ba8483799
Sean Candon <candons@tcd.ie>
2018-09-24 22:29:01 +0100

    added test_printDepthFirst method

--- test_lca.py
+++ test_lca.py
@@ -23,3 +23,14 @@ class TestLCA(unittest.TestCase):
     def test_graphOneNode(self):
         root = lca.Node(1)
         self.assertEqual((lca.findLCA(root, 1, 1)), 1)
+
+    def test_PrintDepthFirst(self):
+        root = lca.Node(1)
+        root.left = lca.Node(2)
+        root.right = lca.Node(3)
+        root.left.left = lca.Node(4)
+        root.left.right = lca.Node(5)
+        root.right.left = lca.Node(6)
+        root.right.right = lca.Node(7)
+        ans = lca.printDepthFirst()
+        self.assertEqual(ans, "1, 2, 4, 5, 3, 6, 7")
