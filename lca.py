9abbf92ff0481b83c1ea5c91d34463c9d52b0592
Sean Candon <candons@tcd.ie>
2018-09-25 15:18:41 +0100

    modified code to perform as binary search tree, not binary tree

--- lca.py
+++ lca.py
@@ -5,49 +5,111 @@ class Node:
         self.left = None
         self.right = None
 
+    def get_key(self):
+        return self.key
 
-def printTree(n, ans):
+    def set_key(self, key):
+        self.key = key
 
-    if n!=None:
-        ans += [n.key]
+    def get_left(self):
+        return self.left
 
-    if n.left!=None:
-        printTree(n.left, ans)
+    def get_right(self):
+        return self.right
 
-    if n.right!=None:
-        printTree(n.right, ans)
+class BST:
+
+    def ___init__(self):
+        self.root = None
+
+    def set_root(self, key):
+        self.root = Node(key)
+
+    def insert(self, key):
+        if(self.root is None):
+            self.set_root(key)
+        else:
+            self.insert_node(self.root, key)
 
 
-def findPath(root, path, k):
+    def insert_node(self, curr, key):
+        if (key <= curr.key):
+            if(curr.left):
+                self.insert_node(curr.left, key)
+            else:
+                curr.left = Node(key)
+        elif(key > curr.key):
+            if(curr.right):
+                self.insert_node(curr.right, key)
+            else:
+                curr.right = Node(key)
 
-    if root is None:
+    def find(self, key):
+        return self.find_node(self.root, key)
+
+    def find_node(self, curr, key):
+        if(curr is None):
+            return False
+        elif(key is curr.key):
+            return True
+        elif(val < curr.key):
+            return self.find_node(curr.left, key)
+        else:
+            return self.find_node(curr.right, key)
+
+    def print_tree(self, n, ans):
+
+        if n!=None:
+            ans += [n.key]
+
+        if n.left!=None:
+            print_tree(n.left, ans)
+
+        if n.right!=None:
+            print_tree(n.right, ans)
+
+
+    def find_path(self, root, path, k):
+
+        if root is None:
+            return False
+
+        path.append(root.key)
+
+        if root.key == k:
+            return True
+
+        if((root.left!=None and self.find_path(root.left, path, k)) or
+           (root.right!=None and self.find_path(root.right, path, k))):
+            return True
+
+        path.pop()
         return False
 
-    path.append(root.key)
+    def find_lca(self, root, n1, n2):
 
-    if root.key == k:
-        return True
+        path1 = []
+        path2 = []
 
-    if((root.left!=None and findPath(root.left, path, k)) or
-       (root.right!=None and findPath(root.right, path, k))):
-        return True
+        if(not self.find_path(root, path1, n1) or not
+           self.find_path(root, path2, n2)):
+            return -1
 
-    path.pop()
-    return False
+        i=0
+        while(i<len(path1) and i<len(path2)):
+            if(path1[i] != path2[i]):
+                break
+            i+=1
 
-def findLCA(root, n1, n2):
+        return path1[i-1]
 
-    path1 = []
-    path2 = []
 
-    if(not findPath(root, path1, n1) or not
-       findPath(root, path2, n2)):
-        return -1
 
-    i=0
-    while(i<len(path1) and i<len(path2)):
-        if(path1[i] != path2[i]):
-            break
-        i+=1
-
-    return path1[i-1]
+# tree = BST()
+# tree.set_root(1)
+# tree.insert(5)
+# tree.insert(2)
+# tree.insert(8)
+# tree.insert(7)
+# tree.insert(11)
+# tree.insert(4)
--- test_lca.py
+++ test_lca.py
@@ -4,34 +4,35 @@ import lca
 class TestLCA(unittest.TestCase):
 
     def test_simpleFindLCA(self):
-        root = lca.Node(1)
-        root.left = lca.Node(2)
-        root.right = lca.Node(3)
-        root.left.left = lca.Node(4)
-        root.left.right = lca.Node(5)
-        root.right.left = lca.Node(6)
-        root.right.right = lca.Node(7)
-        ans = lca.findLCA(root, 5, 4)
-        self.assertEqual(ans, 2)
-        ans2 = lca.findLCA(root, 5, 7)
-        self.assertEqual(ans2, 1)
+        tree = lca.BST()
+        tree.set_root(1)
+        tree.insert(5)
+        tree.insert(2)
+        tree.insert(8)
+        tree.insert(7)
+        tree.insert(11)
+        tree.insert(4)
+        ans = tree.find_lca(tree.root, 8, 4)
+        self.assertEqual(ans, 5)
+        ans = tree.find_lca(tree.root, 11, 7)
+        self.assertEqual(ans, 8)
 
-    def test_emptyGraph(self):
-        root = None
-        self.assertEqual((lca.findLCA(root, 2, 1)), -1)
 
-    def test_graphOneNode(self):
-        root = lca.Node(1)
-        self.assertEqual((lca.findLCA(root, 1, 1)), 1)
-
-    def test_PrintTree(self):
-        root = lca.Node(1)
-        root.left = lca.Node(2)
-        root.right = lca.Node(3)
-        root.left.left = lca.Node(4)
-        root.left.right = lca.Node(5)
-        root.right.left = lca.Node(6)
-        root.right.right = lca.Node(7)
-        ans = []
-        lca.printTree(root, ans)
-        self.assertEqual(ans, [1, 2, 4, 5, 3, 6, 7])
+    # def test_emptyGraph(self):
+        # root = None
+        # self.assertEqual((lca.findLCA(root, 2, 1)), -1)
+#
+    # def test_graphOneNode(self):
+        # root = lca.Node(1)
+        # self.assertEqual((lca.findLCA(root, 1, 1)), 1)
+#
+    # def test_PrintTree(self):
+        # root = lca.Node(1)
+        # root.left = lca.Node(2)
+        # root.right = lca.Node(3)
+        # root.left.left = lca.Node(4)
+        # root.left.right = lca.Node(5)
+        # root.right.left = lca.Node(6)
+        # root.right.right = lca.Node(7)
+        # ans = []
+        # lca.printTree(root, ans)
