8f04ebc76b8c1d0b0214fe0b768c11d9edc1f373
Sean Candon <candons@tcd.ie>
2018-09-27 11:39:50 +0100

    test for find path method passed

--- test_lca.py
+++ test_lca.py
@@ -29,6 +29,22 @@ class TestLCA(unittest.TestCase):
         self.assertEqual((tree.find_lca(5, 5)),
                          tree.root.key)
 
+    def test_findPath(self):
+        tree = lca.BST()
+        tree.set_root(1)
+        tree.insert(5)
+        tree.insert(2)
+        tree.insert(8)
+        tree.insert(7)
+        tree.insert(11)
+        tree.insert(4)
+        path = []
+        tree.find_path(tree.root, path, 4)
+        self.assertEqual(path, [1, 5, 2, 4])
+        path = []
+        tree.find_path(tree.root, path, 11)
+        self.assertEqual(path, [1, 5, 8, 11])
+
 
     def test_print_tree(self):
         tree = lca.BST()
