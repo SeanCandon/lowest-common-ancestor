99b941a53e33fcf6f172d7747a67d1afeb95e92e
Sean Candon <candons@tcd.ie>
2018-10-08 22:47:52 +0100

    made tests for add_edge

--- src/test_lca.py
+++ src/test_lca.py
@@ -20,6 +20,29 @@ class TestLCA(unittest.TestCase):
         #tests find lca for non-existent node
         self.assertEqual(tree.find_lca(6, 4), -1)
 
+    def test_addEdge(self):
+        g = lca.Graph()
+        g.set_root(1)
+        g.add_edge(1, 2)
+        n = g.get_node(1)
+        self.assertEqual(len(n.children), 1)
+        self.assertEqual(len(n.parents), 0)
+        n = g.get_node(2)
+        self.assertEqual(len(n.children), 0)
+        self.assertEqual(len(n.parents), 1)
+        g.add_edge(1, 3)
+        n = g.get_node(1)
+        self.assertEqual(len(n.children), 2)
+        self.assertEqual(len(n.parents), 0)
+        self.assertEqual(n.children[0].key, 2)
+        self.assertEqual(n.children[1].key, 3)
+        g.add_edge(2, 3)
+        n = g.get_node(3)
+        self.assertEqual(len(n.parents), 2)
+        self.assertEqual(n.parents[0].key, 1)
+        self.assertEqual(n.parents[1].key, 2)
+
+
     #tests lca for empty graph
     def test_emptyGraph(self):
         tree = lca.Graph()
