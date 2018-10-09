4dfda1f8e6e2fdd2033c93ad6296de283c98136c
Sean Candon <candons@tcd.ie>
2018-10-09 15:03:14 +0100

    altered find_path, used it to check for cycles when adding edges

--- src/lca.py
+++ src/lca.py
@@ -35,6 +35,7 @@ class Graph:
                 return n
 
     def add_edge(self, key1, key2):
+
         if key1 not in self.vertices:
             return False
         if key2 not in self.vertices:
@@ -57,7 +58,13 @@ class Graph:
             n2.parents.extend([n1])
             self.nodes.extend([n1])
             self.nodes.extend([n2])
-            return True
+            b = self.find_path(key1, key1)
+            if b == True:
+                n1.children.remove(n2)
+                n2.parents.remove(n1)
+                return False
+            else:
+                return True
 
     def set_root(self, key):
         self.root = Node(key)
@@ -121,56 +128,103 @@ class Graph:
             self.__print_tree(n.right, ans)
 
     #find path to node from root, called by findLCA
-    def find_path(self, path, k):
+    def find_path(self, k1, k2):
 
-        if self.size is 0:
+        print("finding path from %d to %d" % (k1, k2))
+
+        n1 = self.get_node(k1)
+        n2 = self.get_node(k2)
+
+        visited = list()
+        first = True
+
+        if(n1 is None or n2 is None):
+            print("wow")
             return False
         else:
-            return self.__find_path(self.root, path, k)
+            print("wow2")
+            visited.extend([k2])
+            return self.__find_path(k2, n1, visited, first)
+        #if self.size is 0:
+        #    return False
+        #else:
+        #    return self.__find_path(self.root, path, k)
 
     #private find path recursive method
-    def __find_path(self, root, path, k):
+    def __find_path(self, k, node, v, first):
 
+        if node.key not in v:
+            if node.key == k:
+                print("path found")
+                return True
+
+        if node.key not in v:
+            v.extend([node.key])
+
+        if first is True:
+            first = False
+
+        children = node.children
+
+        found = False
+
+        for n in children:
+            if n.key == k:
+                print("path found")
+                return True
+            elif n.key not in v:
+                print("check %d" % n.key)
+                found = self.__find_path(k, n, v, first)
+                if found is True:
+                    return True
+
+        #if len(v) == len(self.vertices):
+        return False
+        #else:
+        #    return True
         #if root is None:
             #return False
 
-        path.append(root.key)
+        #path.append(root.key)
 
-        if root.key == k:
-            return True
+        #if root.key == k:
+        #    return True
 
-        if((root.left!=None and self.__find_path(root.left, path, k)) or
-           (root.right!=None and self.__find_path(root.right, path, k))):
-            return True
+        #if((root.left!=None and self.__find_path(root.left, path, k)) or
+         #  (root.right!=None and self.__find_path(root.right, path, k))):
+        #    return True
 
-        path.pop()
-        return False
+        #path.pop()
+        #return False
 
     #method to find lowest common ancestor
     #of two nodes n1 and n2
-    def find_lca(self, n1, n2):
+    #def find_lca(self, n1, n2):
 
-        if self.root is None:
-            return -1
+    #    if self.root is None:
+    #        return -1
 
-        path1 = []
-        path2 = []
+    #    path1 = []
+    #    path2 = []
 
-        if(not self.find_path(path1, n1) or not
-           self.find_path(path2, n2)):
-            return -1
+    #    if(not self.find_path(path1, n1) or not
+    #       self.find_path(path2, n2)):
+    #        return -1
 
-        i=0
-        while(i<len(path1) and i<len(path2)):
-            if(path1[i] != path2[i]):
-                break
-            i+=1
+    #    i=0
+    #    while(i<len(path1) and i<len(path2)):
+    #        if(path1[i] != path2[i]):
+    #            break
+    #        i+=1
 
-        return path1[i-1]
+    #    return path1[i-1]
 
 
 g = Graph()
 g.set_root(1)
-g.add_edge(1, 2)
-v = g.vertices
-print(*v)
+b = g.add_edge(1, 2)
+#print(b)
+#print(g.find_path(1, 1))
+#b = g.add_edge(2, 1)
+#print(g.find_path(1, 1))
+#print(b)
--- src/test_lca.py
+++ src/test_lca.py
@@ -3,23 +3,6 @@ import lca
 
 class TestLCA(unittest.TestCase):
 
-    #tests find lca for basic graph
-    def test_simpleFindLCA(self):
-        tree = lca.Graph()
-        tree.set_root(1)
-        tree.insert(5)
-        tree.insert(2)
-        tree.insert(8)
-        tree.insert(7)
-        tree.insert(11)
-        tree.insert(4)
-        ans = tree.find_lca(8, 4)
-        self.assertEqual(ans, 5)
-        ans = tree.find_lca(11, 7)
-        self.assertEqual(ans, 8)
-        #tests find lca for non-existent node
-        self.assertEqual(tree.find_lca(6, 4), -1)
-
     def test_addEdge(self):
         g = lca.Graph()
         g.set_root(1)
@@ -42,102 +25,21 @@ class TestLCA(unittest.TestCase):
         self.assertEqual(n.parents[0].key, 1)
         self.assertEqual(n.parents[1].key, 2)
 
-
-    #tests lca for empty graph
-    def test_emptyGraph(self):
-        tree = lca.Graph()
-        self.assertEqual((tree.find_lca(2, 1)), -1)
-
-    #tests lca for graph of size 1
-    def test_graphOneNode(self):
-        tree = lca.Graph()
-        tree.set_root(5)
-        self.assertEqual((tree.find_lca(5, 5)),
-                         tree.root.key)
-
-    #tests find path
-    def test_findPath(self):
-        tree = lca.Graph()
-        tree.insert(1)
-        tree.insert(5)
-        tree.insert(2)
-        tree.insert(8)
-        tree.insert(7)
-        tree.insert(11)
-        tree.insert(4)
-        path = []
-        #tests find path to key 4
-        tree.find_path(path, 4)
-        self.assertEqual(path, [1, 5, 2, 4])
-        path = []
-        #tests find path to key 11
-        tree.find_path(path, 11)
-        self.assertEqual(path, [1, 5, 8, 11])
-        path = []
-        #tests find path to non-existent key
-        self.assertEqual(tree.find_path(path, 6), False)
-
-    #tests find path for empty graph, expected false
-    def test_find_pathEmpty(self):
-        tree = lca.Graph()
-        path = []
-        self.assertEqual(tree.find_path(path, 3), False)
-        self.assertEqual(path, [])
-
-    #tests print tree for empty tree
-    def test_printTreeEmpty(self):
-        tree = lca.Graph()
-        ans = []
-        tree.print_tree(ans)
-        self.assertEqual(ans, [])
-
-    #tests print tree for graph of size 1
-    def test_printTreeOneNode(self):
-        tree = lca.Graph()
-        tree.set_root(1)
-        ans = []
-        tree.print_tree(ans)
-        self.assertEqual(ans, [1])
-
-    #tests print tree for standard tree of size>1
-    def test_print_tree(self):
-        tree = lca.Graph()
-        tree.set_root(1)
-        tree.insert(5)
-        tree.insert(2)
-        tree.insert(8)
-        tree.insert(7)
-        tree.insert(11)
-        tree.insert(4)
-        ans = []
-        tree.print_tree(ans)
-        self.assertEqual(ans, [1, 5, 2, 4, 8, 7, 11])
-
-    #tests find for key that is present in tree,
-    #should be true
-    def test_findTrue(self):
-        tree = lca.Graph()
-        tree.set_root(1)
-        tree.insert(5)
-        tree.insert(2)
-        tree.insert(8)
-        tree.insert(7)
-        tree.insert(11)
-        tree.insert(4)
-        n = tree.find(7)
-        assert n == True
-        self.assertEqual(tree.find(2), True)
-
-    #tests find for key that isn't present in tree,
-    #should be false
-    def test_findFalse(self):
-        tree = lca.Graph()
-        self.assertEqual(tree.find(1), False)
-        tree.set_root(1)
-        tree.insert(5)
-        tree.insert(2)
-        tree.insert(8)
-        tree.insert(7)
-        tree.insert(11)
-        tree.insert(4)
-        self.assertEqual(tree.find(6), False)
+    def test_addEdgeWithCycles(self):
+        g = lca.Graph()
+        g.set_root(1)
+        b = g.add_edge(1, 2)
+        self.assertEqual(b, True)
+        b = g.add_edge(1, 3)
+        self.assertEqual(b, True)
+        b = g.add_edge(2, 3)
+        self.assertEqual(b, True)
+        b = g.add_edge(3, 1)
+        self.assertEqual(b, False)
+        b = g.add_edge(2, 4)
+        self.assertEqual(b, True)
+        self.assertEqual((g.get_node(2)).children[0].key, 3)
+        self.assertEqual((g.get_node(2)).children[1].key, 4)
+        self.assertEqual(len(g.get_node(2).children), 2)
+        b = g.add_edge(4, 1)
+        self.assertEqual(b, False)
