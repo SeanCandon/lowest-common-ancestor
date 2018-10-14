2947a0274abdfc4d7c231feb4a13ca4600dd0015
Sean Candon <candons@tcd.ie>
2018-10-14 17:27:15 +0100

    code coverage 100%

--- src/lca.py
+++ src/lca.py
@@ -3,20 +3,9 @@ class Node:
     #and some getters and setters
     def  __init__(self, key):
         self.key = key
-        self.left = None
-        self.right = None
         self.parents = list()
         self.children = list()
         self.depth = None
-
-    def get_key(self):
-        return self.key
-
-    def get_left(self):
-        return self.left
-
-    def get_right(self):
-        return self.right
 
 #graph class
 class Graph:
@@ -35,7 +24,7 @@ class Graph:
             if n.key == key1:
                 return n
 
-    def add_edge(self, key1, key2):
+    def insert(self, key1, key2):
 
         if key1 not in self.vertices:
             return False
@@ -43,14 +32,11 @@ class Graph:
             self.vertices.extend([key2])
             n = Node(key2)
             par = self.get_node(key1)
-            if par != None:
-                n.parents.extend([par])
-                par.children.extend([n])
-                self.nodes.extend([n])
-                n.depth = par.depth+1
-                return True
-            else:
-                return False
+            n.parents.extend([par])
+            par.children.extend([n])
+            self.nodes.extend([n])
+            n.depth = par.depth+1
+            return True
         else:
             n1 = self.get_node(key1)
             n2 = self.get_node(key2)
@@ -79,97 +65,24 @@ class Graph:
         self.nodes.extend([self.root])
         self.root.depth = 0
 
-    #insert key into tree, calls private insert method
-    def insert(self, key):
-        if(self.root is None):
-            self.set_root(key)
-        else:
-            self.__insert(self.root, key)
-            self.size+=1
-
-    #private insert recursive method
-    def __insert(self, curr, key):
-        if (key <= curr.key):
-            if(curr.get_left()):
-                self.__insert(curr.get_left(), key)
-            else:
-                curr.left = Node(key)
-        elif(key > curr.get_key()):
-            if(curr.get_right()):
-                self.__insert(curr.get_right(), key)
-            else:
-                curr.right = Node(key)
-
-    #find key in tree, calls private recursive method
-    def find(self, key):
-        return self.__find(self.root, key)
-
-    #private recursive find method
-    def __find(self, curr, key):
-        if(curr is None):
-            return False
-        elif(key is curr.key):
-            return True
-        elif(key < curr.key):
-            return self.__find(curr.left, key)
-        else:
-            return self.__find(curr.right, key)
-
-    #print tree depth first, calls privte method
-    def print_tree(self, ans):
-        if self.size is 0:
-            ans = []
-        else:
-            self.__print_tree(self.root, ans)
-
-    #private print method
-    def __print_tree(self, n, ans):
-
-        if n!=None:
-            ans += [n.key]
-
-        if n.left!=None:
-            self.__print_tree(n.left, ans)
-
-        if n.right!=None:
-            self.__print_tree(n.right, ans)
-
-    #find path to node from root, called by findLCA
+    #find path to node with key k2 from node with key k1
+    #used to check for cycles
+    #calls private recursive method __find_path
     def find_path(self, k1, k2):
-
-        print("finding path from %d to %d" % (k1, k2))
 
         n1 = self.get_node(k1)
         n2 = self.get_node(k2)
 
         visited = list()
-        first = True
 
-        if(n1 is None or n2 is None):
-            print("wow")
-            return False
-        else:
-            print("wow2")
-            visited.extend([k2])
-            return self.__find_path(k2, n1, visited, first)
-        #if self.size is 0:
-        #    return False
-        #else:
-        #    return self.__find_path(self.root, path, k)
+        visited.extend([k2])
+        return self.__find_path(k2, n1, visited)
 
     #private find path recursive method
-    def __find_path(self, k, node, v, first):
-
-        if node.key not in v:
-            if node.key == k:
-                print("path found")
-                return True
+    def __find_path(self, k, node, v):
 
         if node.key not in v:
             v.extend([node.key])
-
-        if first is True:
-            first = False
 
         children = node.children
 
@@ -177,32 +90,13 @@ class Graph:
 
         for n in children:
             if n.key == k:
-                print("path found")
                 return True
             elif n.key not in v:
-                print("check %d" % n.key)
-                found = self.__find_path(k, n, v, first)
+                found = self.__find_path(k, n, v)
                 if found is True:
                     return True
 
-        #if len(v) == len(self.vertices):
         return False
-        #else:
-        #    return True
-        #if root is None:
-            #return False
-
-        #path.append(root.key)
-
-        #if root.key == k:
-        #    return True
-
-        #if((root.left!=None and self.__find_path(root.left, path, k)) or
-         #  (root.right!=None and self.__find_path(root.right, path, k))):
-        #    return True
-
-        #path.pop()
-        #return False
 
     #method to find lowest common ancestor
     #of two nodes with keys k1 and k2
@@ -234,6 +128,7 @@ class Graph:
 
         return lca
 
+    #finds ancestors of node orig
     def __find_ancestors(self, n, orig, anc):
 
         par = n.parents
@@ -247,21 +142,3 @@ class Graph:
                 self.__find_ancestors(p, orig, anc)
 
         return True
-
-
-g = Graph()
-g.set_root(1)
-g.add_edge(1, 2)
-g.add_edge(1, 3)
-g.add_edge(2, 3)
-g.add_edge(2, 4)
-g.add_edge(2, 5)
-g.add_edge(5, 3)
-g.add_edge(3, 6)
-g.add_edge(6, 7)
-print(g.find_lca(4, 7))
-#print(b)
-#print(g.find_path(1, 1))
-#b = g.add_edge(2, 1)
-#print(g.find_path(1, 1))
-#print(b)
